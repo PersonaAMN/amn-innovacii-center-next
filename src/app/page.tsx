c9e9f5183819b6648941034cc91e38419d2dacc8
aeternologistLaptop <aeternologist@proton.me>
2023-07-21 20:10:30 +0500

    message

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Header } from '@/components/header/header'
+import Image from 'next/image'
+import { memo } from 'react'
+
+export default function Home() {
+  return (<>
+  <div className='pb-16'>bye</div>
+  <div className="text-day-00 lg:text-[92px]">help</div>
+  <Header />
+  </>
+  )
+}
