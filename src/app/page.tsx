b61382f62428eb3ebf824c2e15f9930fd7ded3f2
aeternologistLaptop <aeternologist@proton.me>
2023-07-25 19:11:17 +0500

    Testing useMemo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
+
 import { Header } from '@/components/header/header'
 import Image from 'next/image'
 import { memo } from 'react'
+import { Tester } from '../components/tester/tester';
 
 export default function Home() {
   return (<>
   <Header />
   <div className='pb-16'>bye</div>
   <div className="text-day-00 lg:text-[92px]">help</div>
+  <Tester />
   </>
   )
 }
