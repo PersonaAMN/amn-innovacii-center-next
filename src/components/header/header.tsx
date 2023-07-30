e33fb98ae549d65d8bf63271d1985fd7cfa8b728
aeternologistLaptop <aeternologist@proton.me>
2023-07-30 13:33:53 +0500

    don't understand why i need server and cli

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -9,15 +9,17 @@ export const Header = (props: Props) => {
     const CustomButton = (props: ButtonHTMLAttributes<HTMLButtonElement>) => {
         return <button {...props} />;
     }
+
+    const isClient = typeof window !== "undefined"
      
 
     return (
         <header className="flex justify-between text-[36px] leading-[40px] items-center h-[148px] lg:h-[162px] px-16">
-            <span>О нас</span>
+            <CustomButton type="button" disabled={false} className="hello-button" onClick={isClient ? (() => console.log("hello")) : undefined}>О нас</CustomButton>
             <span>Преподаватели</span>
             <Image src="/logo.jpeg" alt="Учебный центр" width={162} height={162}/>
             <span>Программы</span>
             <span>Контакты</span>
         </header>
     );
-}
+}   
