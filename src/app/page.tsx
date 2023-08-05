3731363a28197552ab555f3bee9ddb59d931edca
aeternologistLaptop <aeternologist@proton.me>
2023-08-05 23:32:53 +0500

    change button to Link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,17 @@
 "use client";
 
-import { Header } from "@/components/header/header";
 import Image from "next/image";
 import { memo } from "react";
 import { experts } from "@/components/catalog/expertsInfo";
 import About from "@/components/about/about";
 import CardsList from "@/components/catalog/expertCard";
-import Index from "@/components/footer/footer";
+import Index from "@/components/footer/Footer";
 
 export default function Home() {
   return (
     <>
-      <Header title="Hello Sandbox" />
       <About />
       <CardsList />
-      <Index />
     </>
   );
 }
